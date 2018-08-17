b52e367018d051b0db050286b3abde46ceaf9a18
Nikoloz Chitashvili <nika@wifisher.com>
2018-08-17 17:33:16 +0400

    Rewrote Tool For OOP

--- webmoon.py
+++ webmoon.py
@@ -2,35 +2,38 @@ import urllib3
 import colorama
 from colorama import *
 import socket
-
+import argparse
+import sys
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 colorama.init()
 
-def parse_wordlist(path):
-    wordlist = open(path, 'r+')
-    word_list_py = []
-    for i in wordlist:
-        word_list_py.append(i.rstrip())
-    return word_list_py
 
-def main(url, wordlist_path = "N:\\Tools\\webmoon\\rockyou.txt"):
-        fucker = {
-            'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36',
-            'Accept-Language': 'en-us',
-            'Accept-Encoding': 'identity',
-            'Keep-Alive': '300',
-            'Connection': 'keep-alive',
-            'Cache-Control': 'max-age=0',
-        }
-        black_list = []
-        listt = parse_wordlist(wordlist_path)
-        print("Operation Starting Boss..............")
-        for i in listt:
+class SubDomain:
+
+    def __init__(self, url, wordlist):
+        self.url = url
+        self.wordlist = wordlist
+    def main(self):
+        try:
+            socket.gethostbyname(self.url)
+        except socket.gaierror:
+            print("\nCouldn't Resolve DNS")
+            sys.exit(0)
+        http = urllib3.PoolManager()
+
+        headers = {
+                    'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36',
+                    'Accept-Language': 'en-us',
+                    'Accept-Encoding': 'identity',
+                    'Keep-Alive': '300',
+                    'Connection': 'keep-alive',
+                    'Cache-Control': 'max-age=0',
+                }
+        black_list = list()
+        for i in open(self.wordlist, 'r'):
             try:
-                new_url = "https://" + str(i).rstrip() + "." + url
-                http = urllib3.PoolManager()
-                # print('Sent:', new_url)
-                r = http.request('POST', new_url, headers=fucker)
+                new_url = 'https://' + i.rstrip() + '.' + self.url
+                r = http.request('GET', new_url)
                 if r.status >= 200 and r.status < 300:
                     color_status = Fore.GREEN
                 else:
@@ -39,47 +42,27 @@ def main(url, wordlist_path = "N:\\Tools\\webmoon\\rockyou.txt"):
                     print(Fore.BLUE + "URL: " + Fore.RED + new_url + Fore.BLUE + " | IP: " + Fore.RED+socket.gethostbyname(new_url.split("://")[1]) + Fore.BLUE +" | Response: " + color_status + str(r.status))
                     black_list.append(new_url)
             except urllib3.exceptions.MaxRetryError:
-                # print(r.status)
                 pass
             except UnicodeError:
                 pass
-            except urllib3.exceptions.ProtocolError:
-            	pass
             except KeyboardInterrupt:
-            	with open('report.txt', 'w') as file:
-            		for i in black_list:
-            			file.write(i+'\n')
-
-
-def validateUrl(url):
-    domain_extensions = ['com', 'ge', 'io', 'de', 'ru']
-    try:
-        if url.split(".")[1] not in domain_extensions:
-            print("URL Extension Doesn't Seem To Be Valid")
-            url = input("Please Enter Valid The URL: ")
-            validateUrl(url)
-    except IndexError:
-        print("You Entered URL Without Extension")
-        url = input("Please Enter Valid The URL: ")
-        validateUrl(url)
-    return url
-
+                print(Fore.RED + "\nQuiting...")
+                sys.exit(0)
+            except urllib3.exceptions.ProtocolError:
+                pass
 
 if __name__ == '__main__':
-    # parser = argparse.ArgumentParser(description='DNS Scanner To Get Subdomains of Website')
-    # parser.add_argument('-u', '--url', help="URL To Scan")
-    # # parser.add_argument('-w', '--wordlist', help="Import Your Wordlist")
-    # args = parser.parse_args()
-    # # print(args)
-    # # exit()
-    # if not args.url:
-    #     print("\nPlease Specify The Url And Start The Program Again!")
-    #     print("Use -h for Help")
-    #     sys.exit(-1)
-    # # elif args.wordlist:
-    # #     main(url=args.url, word_list=args.wordlist)
-    # else:
-    #     main(url=args.url)
+    parser = argparse.ArgumentParser(description='DNS Scanner To Get Subdomains of Website')
+    parser.add_argument('-u', '--url', help="URL To Scan")
+    parser.add_argument('-w', '--wordlist', help="Select Wordlist")
+    args = parser.parse_args()
+    if not args.url:
+        print("\nURL Input Error")
+        sys.exit(0)
+    if not args.wordlist or not args.wordlist.endswith(".txt"):
+        print("\nWordlist Input Error")
+        sys.exit(0)
+
     print(Fore.BLUE + '''
  ▄█     █▄     ▄████████ ▀█████████▄    ▄▄▄▄███▄▄▄▄    ▄██████▄   ▄██████▄  ███▄▄▄▄   
 ███     ███   ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄ ███    ███ ███    ███ ███▀▀▀██▄ 
@@ -89,6 +72,10 @@ if __name__ == '__main__':
 ███     ███   ███    █▄    ███    ██▄ ███   ███   ███ ███    ███ ███    ███ ███   ███ 
 ███ ▄█▄ ███   ███    ███   ███    ███ ███   ███   ███ ███    ███ ███    ███ ███   ███ 
  ▀███▀███▀    ██████████ ▄█████████▀   ▀█   ███   █▀   ▀██████▀   ▀██████▀   ▀█   █▀ 
+ 
+    ╔╗ ┬ ┬  ╔╗╔┬┌─┐┬┌─  ╔═╗┬ ┬┬┌┬┐┌─┐┌─┐┬ ┬┬  ┬┬┬  ┬
+    ╠╩╗└┬┘  ║║║││  ├┴┐  ║  ├─┤│ │ ├─┤└─┐├─┤└┐┌┘││  │
+    ╚═╝ ┴   ╝╚╝┴└─┘┴ ┴  ╚═╝┴ ┴┴ ┴ ┴ ┴└─┘┴ ┴ └┘ ┴┴─┘┴
     ''')
-    url = input("Please Enter The URL: ")
-    main(validateUrl(url))
+    lets_do_this = SubDomain(url=args.url, wordlist=args.wordlist)
+    lets_do_this.main()
